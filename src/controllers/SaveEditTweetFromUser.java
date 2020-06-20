eeec7cd3f1bbc7672f550c20cfdc655880dc20ab
angelhd1999 <angelhd1999@gmail.com>
2020-06-20 13:48:07 +0200

    Update SaveEditTweetFromUser.java

--- src/controllers/SaveEditTweetFromUser.java
+++ src/controllers/SaveEditTweetFromUser.java
@@ -6,10 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import managers.ManageTweet;
-import models.User;
 
 /**
  * Servlet implementation class SaveEditTweetFromUser
